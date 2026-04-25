5628c56223c7c77d44666c68cfee2387977b1903
11249a192 <11249a192@kanchiuniv.ac.in>
2026-04-25 19:29:31 +0530

    implementation of shortest path algorithm in c
    Implemented Dijkstra's algorithm for finding the shortest path in a graph.

--- Dijkstras.c
+++ Dijkstras.c
@@ -1,3 +1,17 @@
+AIM:
+to implement of shortest path  graph using dijkstras algrothim.
+
+Algorithm :
+1. Initialize a dist array where dist[source] = 0 and all others are ∞. 
+2. Maintain a set of unvisited vertices. 
+3. While there are unvisited vertices: 
+o Select the vertex u with the minimum dist[u]. 
+o For each neighbor v of u:  
+▪ If dist[u] + weight(u, v) < dist[v]:  
+▪ Update dist[v] (Relaxation). 
+4. Mark u as visited. 
+
+PROGRAM:
 #include <stdio.h>
 #define V 5
 #define INF 999999
