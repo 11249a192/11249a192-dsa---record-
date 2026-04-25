533328b68bb854143ac7ec78d6ea8cffb39f9f01
11249a192 <11249a192@kanchiuniv.ac.in>
2026-04-25 19:16:03 +0530

    implementation of miminum  spanning tree(krushals) in c
    Added AIM and steps for Kruskal's Algorithm to the file.

--- DAA/kruskals.c
+++ DAA/kruskals.c
@@ -1,13 +1,15 @@
-Prim’s Algorithm :
+AIM:
+To implement the minimum spanning tree of a given weighted connected graph by selecting edges in increasing order of weight without forming cycles by the krushals algorithm
 
-1. Start with any vertex  
-2. Mark it as visited  
-3. Look at all edges from visited vertices  
-4. Consider only edges going to unvisited vertices  
-5. Pick the smallest (minimum weight) edge  
-6. Add that edge and the new vertex to the tree  
-7. Repeat steps 3–6  
-8. Continue until all vertices are included 
+Kruskal’s Algorithm :
+
+1. List all edges of the graph  
+2. Sort edges in increasing order of weight  
+3. Pick the smallest edge  
+4. Add it to the tree  
+5. Pick the next smallest edge  
+6. Add it only if it does NOT form a cycle  
+7. Repeat until (V − 1) edges are selected  
 
 PROGRAM:
 
