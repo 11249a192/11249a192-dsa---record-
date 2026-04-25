9a140f8718cbf6d6d79ed712dbac861ad7ef6487
11249a192 <11249a192@kanchiuniv.ac.in>
2026-04-25 21:17:06 +0530

    Revise Tower of Hanoi algorithm and aim
    Updated the algorithm description and aim for clarity.

--- DAA/Tower Of Hanoi.c
+++ DAA/Tower Of Hanoi.c
@@ -1,16 +1,16 @@
-ALGORITHM:
 
-1. When the problem is reduced to a single disk, move that disk directly from Source to 
-Destination, and return.  Base Condition. 
-2. Move Top (n−1) Disks from Source to Auxiliary / Temporary rod using Des na on as temporary 
-storage. 
-3. Move the remaining disk from Source to Destination (Only one disk movement) 
-4. Move (n−1) Disks from Auxiliary / Temporary to Des na on rod using Source as temporary 
-storage 
+AIM:
+To implement tower of hanoi using recursion to move n disks from sourse to destination rod
+
+ALGORITHM:
+1. check the  Base Condition. 
+2. Move Top (n−1) Disks from Source to Auxiliary
+3. Move the largest disk (nth disk) to destination
+4. Move (n−1) Disks from Auxiliary  to destination
 5. Repeat Recursively until base condition satisfied. 
 
-PROGRAM:
 
+PROGRAM:
 #include<stdio.h>
 void hanoi(int n,char s,char a,char d)
 {
